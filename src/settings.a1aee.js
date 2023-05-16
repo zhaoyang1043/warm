dfe75a435a88d930bd800b4da423ace1d7e78628
zhaoyang1043 <133746541+zhaoyang1043@users.noreply.github.com>
2023-05-16 15:46:30 +0800

    Add files via upload

---
+++ src/settings.a1aee.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/01-shouye/01-shouye.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"8c610"}};
